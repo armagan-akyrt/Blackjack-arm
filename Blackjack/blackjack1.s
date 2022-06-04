35821e4875268009866a9f7e1ed985658762555d
Alperen <ozkayaal@mef.edu.tr>
2022-06-04 15:49:33 +0300

    LSL

--- Blackjack/blackjack1.s
+++ Blackjack/blackjack1.s
@@ -15,24 +15,26 @@ _start:
 	// start with value 0
 	MOV R10, #0
 	// assume J and 4 are drawn
+	MOV R8, #10   // J is drawn
 	// timer 
-	LDR R5, [R3, #40]   // 4 * 10   J
+	LDR R5, [R3, R8, LSL #2]   // 4 * 10   J
 	STR R5, [R2] // display the drawn card
-	LDR R7, [R6, #40] // get the value of J
+	LDR R7, [R6, R8, LSL #2] // get the value of J
 	ADD R10, R10, R7
 	
-	LDR R5, [R3, #12]   // 3 * 4   4
+	MOV R8, #3
+	
+	LDR R5, [R3, R8, LSL #2]   // 3 * 4   4
 	STR R5, [R2] // display the drawn card
-	LDR R7, [R6, #12] // get the value of 4
+	LDR R7, [R6, R8, LSL #2] // get the value of 4
 	ADD R10, R10, R7
 	
 	
 	// show the current value
-	LDR R5, [R1, #56]   // J + 4 = 14   ((14)*4)++
+	LDR R5, [R1, R10, LSL #2]   // J + 4 = 14   ((14)*4)++
 	STR R5, [R2, #16] // write to 7-segment
 	
 		 
-		
 	end: B end		
 
 		
@@ -42,3 +44,6 @@ HEXDISPLAYCARDS: .word 0x77, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,0x7F,0x6F, 0x063
 HEXTABLE: .word 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x063F, 0x0606, 0x065B, 0x064F, 0x0666, 0x066D, 0x067D, 0x0607, 0x067F, 0x066F, 0x5B3F, 0x5B06, 0x5B5B, 0x5B4F, 0x5B66, 0x5B6D, 0x5B7D
 // Values of each different cards(total of 13)  A's value should be 11 if the current value is below 10
 CARDVALUES: .word 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xA, 0xA, 0xA, 0xB 
+
+	
+	
