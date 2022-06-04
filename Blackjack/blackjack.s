eb294e188fb8a568e8fbf7de6a88f341a2f11744
Armağan Akyurt <82036765+armagan-akyrt@users.noreply.github.com>
2022-06-04 22:02:51 +0300

    Bugfix

--- Blackjack/blackjack.s
+++ Blackjack/blackjack.s
@@ -9,6 +9,7 @@ _start:
 	MOV R1, #3
 	STR R1, [R0, #8] // Start & loop the timer.
 	MOV R3, #0 // player's value
+	MOV R4, #0 // dealer's value
 	
 	//MOV R2, R3
 	//BL ADDCARD
@@ -64,6 +65,11 @@ _start:
 		LDR R4, =HEXDISPLAYCARDS
 		LDR R5, =CARDVALUES
 		LDR R4, [R4, R1, LSL #2]   // 4 * 10   J
+		//test start
+		LDR R8, [R3]
+		BIC R8, R8, #255
+		ADD R4, R4, R8
+		// test end
 		STR R4, [R3] // display the drawn card
 		BL DO_DELAY
 		
@@ -107,36 +113,3 @@ HEXDISPLAYCARDS: .word 0x77, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,0x7F,0x6F, 0x063
 HEXTABLE: .word 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x063F, 0x0606, 0x065B, 0x064F, 0x0666, 0x066D, 0x067D, 0x0607, 0x067F, 0x066F, 0x5B3F, 0x5B06, 0x5B5B, 0x5B4F, 0x5B66, 0x5B6D, 0x5B7D
 // Values of each different cards(total of 13)  A's value should be 11 if the current value is below 10
 CARDVALUES: .word 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xA, 0xA, 0xA, 0xB 
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
